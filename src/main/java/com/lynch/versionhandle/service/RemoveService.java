93a929a8d7e33e29232edf58d0392ca7b16ad2b1
Hunter Lynch <hunterplynch07@gmail.com>
2026-04-21 18:45:47 +1200

    First implementation of RemoveService done.c

---
+++ src/main/java/com/lynch/versionhandle/service/RemoveService.java
@@ -0,0 +1,66 @@
+package com.lynch.versionhandle.service;
+
+import com.lynch.versionhandle.model.Commit;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Map;
+
+public class RemoveService {
+
+    public static final String DELETED = "<DELETED>";
+
+    /**
+     *
+     * @param repoPath
+     * @param fileName
+     */
+    public void remove(Path repoPath, String fileName) {
+
+        Path vhPath = repoPath.resolve(".versionhandle");
+
+        // Check project is initialised
+        if (!Files.exists(vhPath)) {
+            System.out.print("Not a versionhandle repository. Initialise project first.");
+            return;
+        }
+
+        IndexService indexService = new IndexService();
+        CommitService commitService = new CommitService();
+
+        String currentBranch = commitService.readCurrent(repoPath);
+
+        // Check branch is set
+        if(currentBranch == null) {
+            System.out.println("Error: current branch not set.");
+            return;
+        }
+
+        String currentCommitId = commitService.readBranch(repoPath, currentBranch);
+
+        boolean tracked = false;
+
+        // Check file is tracked
+        if(currentCommitId != null) {
+            Commit current = commitService.loadCommit(repoPath, currentCommitId);
+            tracked = current.getSnapshot().containsKey(fileName);
+        }
+
+        Path filePath = repoPath.resolve(fileName);
+
+        if(!tracked && !Files.exists(filePath)) {
+            System.out.println("File not found: " + fileName);
+        }
+
+        try {
+            Files.deleteIfExists(filePath);
+        } catch(IOException e) {
+            throw new RuntimeException("Failed to delete file: " + fileName);
+        }
+
+        Map<String, String> index = indexService.loadIndex(repoPath);
+
+        System.out.print("File deletion staged: " + fileName);
+    }
+}
